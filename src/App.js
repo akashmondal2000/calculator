917c3a9b5eb3a11407301ceee65c40f37014dfa9
akashmondal2000 <mondalakash494@gmail.com>
2023-02-07 00:00:52 +0530

    Initialize Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
     <h1>Hii i'm akash.webDev</h1>
+    <Wrapper></Wrapper>
 
     </div>
   );
---
+++ src/components/Wrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wrapper = () => {
+  return (
+    <div><h1>Wrapper is work</h1></div>
+  )
+}
+
+export default Wrapper;
