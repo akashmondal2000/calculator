e096b9faaccd324f19fea213f63b596a02331c64
akashmondal2000 <mondalakash494@gmail.com>
2023-02-10 22:29:27 +0530

    import components and initilize button in app.js component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+
 
 function App() {
   return (
@@ -7,6 +10,16 @@ function App() {
     <h1>Hii i'm akash.webDev</h1>
     <Wrapper>
       <Screen/>
+      <ButtonBox>
+        <Button 
+        className="" 
+        value = "1" 
+        onClick={ ()=> {
+          console.log("button clicked") // hare i print button clicked responce on console
+        }}  
+        />
+      </ButtonBox>
+
     </Wrapper>
 
     </div>
