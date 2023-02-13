ecf2a709db75de35c7cd128c82734adb1ddef81b
akashmondal2000 <mondalakash494@gmail.com>
2023-02-14 01:06:57 +0530

    edit the Button component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import React, { useState } from "react";
 
 
 const btnValues = [
@@ -14,11 +15,19 @@ const btnValues = [
 
 
 const App = ()=>{
+
+  const [calc,setCalc] = useState({
+    sign:"",// the selected sign
+    num: 0 ,// the entered value
+    res: 0 ,//the calculated value.
+
+  });
   return (
     <div className="App">
     <h1>Hii i'm akash.webDev</h1>
     <Wrapper>
-      <Screen/>
+      <Screen value = {calc.num? calc.num : calc.res}/>
+
       <ButtonBox>
         {
            btnValues.flat().map((btn,i)=>{
@@ -27,7 +36,15 @@ const App = ()=>{
                 key={i}
                 className = {btn==="=" ? "equal" : ""}
                 value = {btn}
-                onClick = {()=>{console.log(`${btn} Clicked`)}}
+                onClick = {
+                  btn === "C"? resetClickHandler
+                  :btn === "+-"? invertClickHandler
+                  :btn === "%"? percentClickHandler
+                  :btn === "="? equalsClickHandler
+                  :btn === "/" || btn === "X" || btn === "-" || btn === "+"? signClickHandler
+                  :btn === "." ? commaClickHandler 
+                  :numClickHandler
+                }
               />
             )
            })
