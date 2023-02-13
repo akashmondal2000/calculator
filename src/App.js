2a8ca830ccbffea47b428cdc7e8b4bb132df9d3d
akashmondal2000 <mondalakash494@gmail.com>
2023-02-13 14:15:30 +0530

    render all the buttons in the buttonBox

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
 
-function App() {
+const btnValues = [
+  ["C","+-","%","/"],
+  [7,8,9,"X"],
+  [4,5,6,"-"],
+  [1,2,3,"+"],
+  [0,".","="],
+];
+
+
+const App = ()=>{
   return (
     <div className="App">
     <h1>Hii i'm akash.webDev</h1>
     <Wrapper>
       <Screen/>
       <ButtonBox>
-        <Button 
-        className="" 
-        value = "1" 
-        onClick={ ()=> {
-          console.log("button clicked") // hare i print button clicked responce on console
-        }}  
-        />
+        {
+           btnValues.flat().map((btn,i)=>{
+            return (
+              <Button
+                key={i}
+                className = {btn==="=" ? "equal" : ""}
+                value = {btn}
+                onClick = {()=>{console.log(`${btn} Clicked`)}}
+              />
+            )
+           })
+        }
       </ButtonBox>
 
     </Wrapper>
