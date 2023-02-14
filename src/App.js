0b166198d76cb8e09a9e929afa268da7b40ea8c4
akashmondal2000 <mondalakash494@gmail.com>
2023-02-15 02:03:09 +0530

    add equalsClickHandler function

--- src/App.js
+++ src/App.js
@@ -68,6 +68,27 @@ const App = ()=>{
     });
   };
 
+  //equalsClickHandler function
+  const equalsClickHandler = ()=>{
+    if (calc.sign && calc.num ) {
+      const math = (a,b,sign)=>{
+        sign === "+"? a+b
+        : sign === "-" ? a-b
+        : sign === "X" ? a*b : a/b;
+
+        setCalc({
+          ...calc,
+          res:calc.num === "0" && calc.sign ==="/"? 
+          "Can't divide with 0"
+          :math (Number(calc.res),Number(calc.num),calc.sign),
+          sign : "",
+          num : 0,
+        })
+      };
+      
+    }
+  };
+
 
 
 
